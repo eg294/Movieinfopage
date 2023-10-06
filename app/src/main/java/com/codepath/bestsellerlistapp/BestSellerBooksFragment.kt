b98a63f317db970dd7b948db8aad21d21514a452
EGonzalez2 <eg294@njit.edu>
2023-10-06 01:12:31 -0400

    init commmit

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
@@ -1,6 +1,7 @@
 package com.codepath.bestsellerlistapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,20 @@ import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.codepath.bestsellerlistapp.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONArray
+import org.json.JSONObject
 
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = "<YOUR-API-KEY-HERE>"
+private const val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -45,11 +54,16 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
     private fun updateAdapter(progressBar: ContentLoadingProgressBar, recyclerView: RecyclerView) {
         progressBar.show()
 
-        // Create and set up an AsyncHTTPClient() here
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api_key"] = API_KEY
 
-        // Using the client, perform the HTTP request
+        client[
+            "https://api.themoviedb.org/3/movie/now_playing",
+            params,
+            object : JsonHttpResponseHandler()
 
-        /* Uncomment me once you complete the above sections!
+        // Uncomment me once you complete the above sections!
         {
             /*
              * The onSuccess function gets called when
@@ -63,9 +77,10 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
                 // The wait for a response is over
                 progressBar.hide()
 
-                //TODO - Parse JSON into Models
-
-                val models : List<BestSellerBook> = null // Fix me!
+                val resultsJSON : String = json.jsonObject.get("results").toString()
+                val gson = Gson()
+                val arrayBookType = object : TypeToken<List<BestSellerBook>>() {}.type
+                val models : List<BestSellerBook> = gson.fromJson(resultsJSON,arrayBookType)
                 recyclerView.adapter = BestSellerBooksRecyclerViewAdapter(models, this@BestSellerBooksFragment)
 
                 // Look for this in Logcat:
@@ -91,7 +106,7 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
                 }
             }
         }]
-        */
+
 
     }
 
--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -3,8 +3,10 @@ package com.codepath.bestsellerlistapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.R.id
 
 /**
@@ -31,6 +33,11 @@ class BestSellerBooksRecyclerViewAdapter(
         var mItem: BestSellerBook? = null
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mBookDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mposter : ImageView = mView.findViewById<View>(id.posterView) as ImageView
+
+
+
 
         override fun toString(): String {
             return mBookTitle.toString() + " '" + mBookAuthor.text + "'"
@@ -45,7 +52,15 @@ class BestSellerBooksRecyclerViewAdapter(
 
         holder.mItem = book
         holder.mBookTitle.text = book.title
-        holder.mBookAuthor.text = book.author
+        holder.mBookDescription.text = book.description
+
+        val logoUrl = "https://image.tmdb.org/t/p/w500" + book.bookImageUrl
+
+        Glide.with(holder.mView)
+            .load(logoUrl)
+            .centerInside()
+            .into(holder.mposter)
+
 
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
